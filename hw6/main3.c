3df9c9724823f7d5debd7c85a97b98ce606e6539
B1129023 <119866951+B1129023@users.noreply.github.com>
2022-12-22 11:54:10 +0800

    Add files via upload

---
+++ hw6/main3.c
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int year=2022;
+    if(year%4!=0){
+        printf("非閏年");
+    }
+    else{
+        if(year%100!=0){
+            printf("閏年");
+        }
+        else if(year%400==0){
+            printf("閏年");
+        }
+        else{
+            printf("非閏年");
+        }
+    }
+    return 0;
+}
