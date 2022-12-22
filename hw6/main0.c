c61b18e9568a96246ec22d5a5d019d2ca3392c6d
B1129023 <119866951+B1129023@users.noreply.github.com>
2022-12-22 11:33:40 +0800

    Add files via upload

--- hw6/main0.c
+++ hw6/main0.c
@@ -1,1 +1,26 @@
+/******************************************************************************
 
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int i=10;
+    if(i>0){
+        if(i&i-1){
+            printf("%d is false",i);
+        }
+        else{
+            printf("%d is true",i);
+        }
+    }
+    else{
+        printf("%d is false",i);
+    }
+    return 0;
+}
